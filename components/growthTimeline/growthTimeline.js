7f10c208a6cc833dc1bb7b44e912af1846b00328
chengmin <chengmin@qq.com>
2025-11-23 23:19:59 +0800

    ds

--- components/growthTimeline/growthTimeline.js
+++ components/growthTimeline/growthTimeline.js
@@ -5,6 +5,23 @@ Component({
   },
 
   data: {
+    voiceMap: [
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 你好.mp3",
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 你是我的女朋友.mp3",
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 内个菠萝.mp3",
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 再见.mp3",
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 呜呜呜.mp3",
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 对吧.mp3",
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 对对对.mp3",
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 我叫Orm.mp3",
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 我爱你，你爱我吗？.mp3",
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 我爱你.mp3",
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 我爱你哒~.mp3",
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 早安.mp3",
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 明天见！.mp3",
+      "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/vedio/等左左买饼干 - 谢谢.mp3",
+
+    ],
     // Kornnaphat Sethratanapong (Orm) 的成长时间线数据
     timelineData: [
       {
@@ -159,6 +176,9 @@ Component({
       this.setData({
         timelinePoints: timelinePoints
       });
+
+      // 触发播放语音事件
+      this.triggerEvent('playvoice');
     },
 
     // 关闭时间点详情
