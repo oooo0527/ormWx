7ea187e3d5a0f764238cb27269bb65e8ea29615f
chengmin <chengmin@qq.com>
2026-01-05 16:39:30 +0800

    lml

--- packHome/ormHome/ormHome.js
+++ packHome/ormHome/ormHome.js
@@ -12,11 +12,12 @@ Page({
 
   onLoad: function (options) {
     // 页面加载时执行
-    this.initMenu()
+    // this.initMenu()
   },
 
   onShow: function () {
     // 页面显示时执行
+    this.initMenu()
 
   },
   //初始化菜单
