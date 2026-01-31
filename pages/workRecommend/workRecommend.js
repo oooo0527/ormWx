9b99775a0f8fa2a61435001243eae752a7758162
chengmin <chengmin@qq.com>
2026-01-31 14:09:33 +0800

    样式

--- pages/workRecommend/workRecommend.js
+++ pages/workRecommend/workRecommend.js
@@ -20,6 +20,30 @@ Page({
         role: "小翁",
         cover: "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/ormmm/陈奥/2246a8c4f6c263a32bfbb898a3992cc1.jpg",
       },
+      {
+        id: 2,
+        title: "Only You",
+        role: "Ira",
+        cover: "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/ormmm/陈奥/24c44a6355707a277309865e62c1b5cb.jpg",
+      },
+      {
+        id: 3,
+        title: "我们的秘密",
+        role: "尔恩 (Earn)",
+        cover: "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/ormmm/陈奥/329c3f47da4836e2c4ef41bf97540833.jpg",
+      },
+      {
+        id: 4,
+        title: "Potion of Love",
+        role: "Pun",
+        cover: "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/ormmm/陈奥/3916c9499882d66371bc6573597693bf.jpg",
+      },
+      {
+        id: 5,
+        title: "Potion of Love2",
+        role: "Pun",
+        cover: "cloud://cloud1-5gzybpqcd24b2b58.636c-cloud1-5gzybpqcd24b2b58-1387507403/ormmm/陈奥/443aaee45d2852f42a20789b76793ea0.jpg",
+      }
     ]
   },
 
